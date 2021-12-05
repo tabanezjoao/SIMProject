f555ec997530d5fca732e9546b91dd86083219be
mykebueno <71532159+mykebueno@users.noreply.github.com>
2021-12-05 15:58:28 +0000

    Register layout updated and code for registrarion added

--- FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.fitnessapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
+    }
+
+    fun getUserRegistration(view: View)
+    {
+        val username = findViewById<TextView>(R.id.editTextUsername).text.toString()
+        val password = findViewById<TextView>(R.id.editTextPassword).text.toString()
+        val email = findViewById<TextView>(R.id.editTextEmail).text.toString()
+        val phone = findViewById<TextView>(R.id.editTextPhone).text.toString()
+        Log.d("username", username)
+        Log.d("password", password)
+        Log.d("email", email)
+        Log.d("phone", phone)
     }
 }
